4cefd3eb97fc7a483f1527295aa983026f8833db
Santiago Mancera <dsmr19961@gmail.com>
2024-04-16 15:20:14 -0500

    Ajuste MainWindow.js

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -5,9 +5,9 @@ import javax.swing.WindowConstants;
 
 public class MainWindow extends JFrame{
     // Constantes globales
-    final public static int MAIN_WINDOW_WIDTH = 500;
-    final public static int MAIN_WINDOW_HEIGHT = 500;
-    final public static String MAIN_WINDOW_TITLE = "Inventario de automoviles (por ahora 😀)";
+    final private int MAIN_WINDOW_WIDTH = 500;
+    final private int MAIN_WINDOW_HEIGHT = 500;
+    final private String MAIN_WINDOW_TITLE = "Inventario de automoviles (por ahora 😀)";
 
 
     public MainWindow(){
@@ -19,8 +19,8 @@ public class MainWindow extends JFrame{
     // Configuracion
     private void initSettings(){
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setTitle(MAIN_WINDOW_TITLE);
-        this.setSize(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+        this.setTitle(this.MAIN_WINDOW_TITLE);
+        this.setSize(this.MAIN_WINDOW_WIDTH, this.MAIN_WINDOW_HEIGHT);
     }
 
     // Componentes que tiene la ventana (Botones, inpunts, etc)
